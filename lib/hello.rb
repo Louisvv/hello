9092c811fb9a5b9226e7dad64c2b4908e408f36e
闫威 <648555416@@qq.com>
2016-07-10 00:21:13 +0800

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,8 @@
+require 'greeter'
+
 #Deafault is "World"
 #Author:Yanwei (648555416@qq.com)
 name = ARGV.first || ""World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
